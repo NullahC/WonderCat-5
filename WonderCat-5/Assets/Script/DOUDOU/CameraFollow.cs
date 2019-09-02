599d4f23119376d912d4956dc3365eab41faac30
窦豆 <52514351+GJdoudou@users.noreply.github.com>
2019-09-02 11:17:08 +0800

    摄像机跟随

---
+++ WonderCat-5/Assets/Script/DOUDOU/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Cat;
+    private float distanceX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distanceX = transform.position.x - Cat.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+           Cat.position.x + distanceX,
+           transform.position.y,
+           transform.position.z);
+    }
+}
