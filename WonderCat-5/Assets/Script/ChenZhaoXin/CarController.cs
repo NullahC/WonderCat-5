a680a6bf70e32c194850dff43bd4b5f5bffee5f7
陈兆昕 <52514359+NullahC@users.noreply.github.com>
2019-09-02 15:10:52 +0800

    小车完成

---
+++ WonderCat-5/Assets/Script/ChenZhaoXin/CarController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private WheelJoint2D []Wheel = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Wheel =GetComponentsInChildren<WheelJoint2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+
+    }
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+        Wheel[0].useMotor = true;
+        Wheel[1].useMotor = true;
+    }
+}
---
+++ WonderCat-5/Assets/Script/ChenZhaoXin/CarWheelsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarWheelsController : MonoBehaviour
+{
+    private WheelJoint2D wheel = null;
+    private bool a = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(a);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        a = true;
+    }
+}
