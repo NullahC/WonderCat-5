6c8e64d604c9aaa468fd535277ec85c5e7a46fe6
窦豆 <52514351+GJdoudou@users.noreply.github.com>
2019-09-03 10:47:00 +0800

    预制件修改

---
+++ WonderCat-5/Assets/Script/DOUDOU/ResverserController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ResverserController : MonoBehaviour
+{
+    private WheelJoint2D[] Wheel = null;
+    private JointMotor2D left;
+    private JointMotor2D right;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Wheel = GetComponentsInChildren<WheelJoint2D>();
+        left = Wheel[0].motor;
+        right = Wheel[1].motor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag( "Resverser"))
+    //    {
+    //        transform.rotation = Quaternion.Euler(Vector3.up*180);
+    //    }
+    //}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name=="Reverser")
+        {
+            left.motorSpeed = -left.motorSpeed;
+            right.motorSpeed = -right.motorSpeed;
+        }
+        
+    }
+}
