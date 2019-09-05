0884d236e3277a1a64d2370a25323fe4ca5c0d3c
张英 <1651719953@qq.com>
2019-09-05 10:18:37 +0800

    改动
    猫，钻石，按键波

--- WonderCat-5/Assets/Script/ZY/diamond.cs
+++ WonderCat-5/Assets/Script/ZY/diamond.cs
@@ -1,29 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class diamond : MonoBehaviour
 {
-    public int Count = 0;
-   // public AudioClip diamondCollect;
+
+
+    public int Count;
+        private ParticleSystem DiamondPartical;
+
+
+
+    // public AudioClip diamondCollect;
     // Start is called before the first frame update
     void Start()
     {
-        
+        DiamondPartical = GetComponent<ParticleSystem>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("diamond"))
+        GameObject obj = collision.gameObject;
+        if (obj.CompareTag("Hero"))
         {
             //播放吃金币音乐
             // AudioSource.PlayClipAtPoint(diamondCollect, Camera.main.transform.position);
-            Destroy(collision.gameObject);
-            Count++;
+            if(!obj.GetComponent<Hero_Controller>().isDead)
+                {
+                Instantiate(DiamondPartical, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
         }
+        //if (collision.gameObject.CompareTag("diamond"))
+        //{
+        //    //播放吃金币音乐
+        //    // AudioSource.PlayClipAtPoint(diamondCollect, Camera.main.transform.position);
+        //    Destroy(collision.gameObject);
+        //    Count++;
+        //}
 
         // Update is called once per frame
 
     }
+
+    
 
     // Update is called once per frame
     void Update()
