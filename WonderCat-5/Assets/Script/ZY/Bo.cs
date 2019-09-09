f219284a24fe16d4d9b64b97fc9c0d9f0d5fd5ff
张英 <1651719953@qq.com>
2019-09-09 15:20:43 +0800

    BO

---
+++ WonderCat-5/Assets/Script/ZY/Bo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bo : MonoBehaviour
+{
+    public GameObject Wave;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        createKW();
+    }
+    private void createKW()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Vector3 mp = Input.mousePosition;
+            Vector3 keyW = Camera.main.ScreenToWorldPoint(mp);
+            Vector3 newW = new Vector3(keyW.x, keyW.y, 0);
+            GameObject KW = Instantiate(Wave, newW, Quaternion.identity);
+        }
+    }
+}
--- WonderCat-5/Assets/Script/ZY/Fellow.cs
+++ WonderCat-5/Assets/Script/ZY/Fellow.cs
@@ -4,69 +4,17 @@ using UnityEngine;
 
 public class Fellow : MonoBehaviour
 {
-    //public LayerMask lay;
-    //public RaycastHit hit;
-    //public GameObject Bo;
-    //public List<GameObject> cubeList;
-    // void Start()
-    //{
-
-    //}
-    // void Update()
-    //{
-    //    if (Input.GetMouseButtonDown(0))
-    //    {
-    //        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-    //        if(Physics.Raycast(ray,out hit, 100, lay))
-    //        {
-    //            GameObject cube = Instantiate(Bo, hit.point, Quaternion.identity) as GameObject;
-    //        }
-    //    }
-    //}
-
-    //    // Start is called before the first frame update
-    //    //需显示的物体
-    public GameObject rightMenu;
-    //    //摄像机
-    public Camera uicamera;
-    //    //开始隐藏物体
+    
+    
     void Start()
     {
-        rightMenu.SetActive(false);
+        //rightMenu.SetActive(false);
+        Destroy(gameObject, 0.5f);
     }
 
     //    // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetMouseButton(1))
-
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            //获得鼠标位置
-            float x = Input.mousePosition.x;
-            float y = Input.mousePosition.y;
-            float z = Input.mousePosition.z;
-            //给需要在鼠标点击位置出现的物体赋值
-            rightMenu.transform.position = new Vector3(x, y, z);
-            //显示物体
-            rightMenu.SetActive(true);
-
-        }
-        //    }
-        // void OnMouseDown()
-        //{
-        //    if(Input.GetMouseButton(1))
-        //    {
-        //        //获得鼠标位置
-        //        float x = Input.mousePosition.x;
-        //        float y = Input.mousePosition.y;
-        //        float z = Input.mousePosition.z;
-        //        //给需要在鼠标点击位置出现的物体赋值
-        //        rightMenu.transform.position = new Vector3(x, y, z);
-        //        //显示物体
-        //        rightMenu.SetActive(true);
-        //    }
-        //}
     }
 }
