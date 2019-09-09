a20aab11b4bc083fa32663311bc978bb41df26d5
张英 <1651719953@qq.com>
2019-09-09 15:20:26 +0800

    最终，Bo

---
+++ WonderCat-5/Assets/Script/ZY/Spring_1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring_1 : MonoBehaviour
+{
+    public iTween.EaseType type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Spring")
+        {
+            Hashtable args = new Hashtable();
+            args.Add("speed", 5.0f);
+            args.Add("path", iTweenPath.GetPath("Spring_1"));
+            args.Add("easeType", type);
+            iTween.MoveTo(gameObject, args);
+        }
+
+    }
+}
